f703af724e994b2b7b3a08cef161fbb0b6efda26
ankitjangir01 <93923854+ankitjangir01@users.noreply.github.com>
2022-05-15 00:10:25 +0530

    Updates

---
+++ precache-manifest.26fdbd6d8bd94eed8520eafeee539235.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "57a43af295c80eeca1b7",
+    "url": "/CWH-text-utils/static/css/main.6fc9d436.chunk.css"
+  },
+  {
+    "revision": "57a43af295c80eeca1b7",
+    "url": "/CWH-text-utils/static/js/main.57a43af2.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/CWH-text-utils/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "23acdff216e3cca4e483",
+    "url": "/CWH-text-utils/static/js/2.23acdff2.chunk.js"
+  },
+  {
+    "revision": "3e8083ab7aa4b82265a8",
+    "url": "/CWH-text-utils/static/js/runtime~main.3e8083ab.js"
+  },
+  {
+    "revision": "146fdecd20de89047633a495c3c903a0",
+    "url": "/CWH-text-utils/index.html"
+  }
+];
